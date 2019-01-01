eaf5ff8d83fccb04738734dc5b2d2f135c10360d
Anwesh43 <anweshthecool0@gmail.com>
2019-01-01 20:14:24 +0530

    adding scale methods

--- PlusBoxStepStage.ts
+++ PlusBoxStepStage.ts
@@ -8,6 +8,26 @@ const strokeFactor = 90
 const sizeFactor = 3
 const color = "#0D47A1"
 
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1/n, maxScale(scale, i, n))
+}
+
+const scaleFactor : Function = (scale : number) : number => Math.floor(this / scDiv)
+
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = (1 - scaleFactor(scale))
+    return (1 - k) / a + k / b
+}
+
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
 class PlusBoxStepStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
